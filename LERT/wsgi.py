83ccc22bcd9ea6032bf9ebd3f53ba17de0d58f2a
RafaGomezGlez <A01733983@itesm.mx>
2022-04-28 12:12:46 -0500

    feat: Gunicorn config files DEV-94

---
+++ LERT/wsgi.py
@@ -0,0 +1,4 @@
+from LERT.prueba import app
+
+if __name__ == "__main__":
+    app.run()
---
+++ gunicorn.conf.py
@@ -0,0 +1,5 @@
+from imp import reload
+bind = "127.0.0.1:8000"
+loglevel= 'debug'
+workers = 4
+reload = True
