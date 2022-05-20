364138d156ba2a926700ec1322ab2bfe872cf144
RafaGomezGlez <A01733983@itesm.mx>
2022-05-20 08:43:15 -0500

    feat: Adding SQLAlchemy and fixing connection issues DEV-94

--- lert_driver_db2/db2/db2_Connection.py
+++ lert_driver_db2/db2/db2_Connection.py
@@ -1,40 +1,40 @@
 import ibm_db
 import ibm_db_dbi
 import os
+from sqlalchemy import *
+import ibm_db_sa
+
+# Definition of ENV variables
+DB_NAME = os.environ.get("DBNAME")        
+DB_HOSTNAME = os.environ.get("DB_HOSTNAME")
+DB_PASSWORD = os.environ.get("DB2INST1_PASSWORD")
+SECURITY = os.environ.get("SECURITY")
+UID = os.environ.get("UID")
+CERTIFICATE = os.environ.get("CERTIFICATE")
 
 class Db2Connection(object):
-    dbName = ""
-    dbHostName = ""
-    dbPassword = ""
-    security = ""
-    uid = ""
-    certificate = ""
+    def __init__(self): 
+        self.dbstring =  f"DATABASE={DB_NAME};HOSTNAME={DB_HOSTNAME};PROTOCOL=TCPIP;PORT=50000;UID={UID};PWD={DB_PASSWORD}"
+        try:
+            self.ibm_db_conn = ibm_db.connect(self.dbstring, '', '')
+            self._validate_connection()
+            conn = ibm_db_dbi.Connection(self.ibm_db_conn)
+            self.cursor = conn.cursor()
+        except Exception as e:
+            print(e)
 
-    def __init__(self):
-        # self.dbName = db_name
-        # self.dbHostName = db_hostname
-        # self.dbPassword =db_password
-        # self.security = security_
-        # self.uid = uid_
-        # self.certificate = certificate_
-
-        #Environment variables
-        self.DB_NAME = os.environ.get("DBNAME")        
-        self.DB_HOSTNAME = os.environ.get("DB_HOSTNAME")
-        self.DB_PASSWORD = os.environ.get("DB2INST1_PASSWORD")
-        self.SECURITY = os.environ.get("SECURITY")
-        self.UID = os.environ.get("UID")
-        self.CERTIFICATE = os.environ.get("CERTIFICATE")
-
-        #self._create_conn()
-
-    def _create_conn(self):
-    
-        conn_str = f"database=${self.DB_NAME};hostname=${self.DB_HOSTNAME};port=32733;security=${self.SECURITY};SSLServerCertificate=${self.CERTIFICATE};uid=${self.UID};pwd=${self.DB_PASSWORD}"
-        
-        self.ibm_db_conn = ibm_db.connect(conn_str, '', '')
-        conn = ibm_db_dbi.Connection(self.ibm_db_conn)
-        self.cursor = conn.cursor()
+    def _create_connection_sqlAlchemy(self):
+        try:
+            e = create_engine(f"db2+ibm_db://{UID}:{DB_PASSWORD}@{DB_HOSTNAME}:50000/{DB_NAME}")
+            self.metadata = MetaData()
+            self.metadata.bind = e
+            
+        except Exception as e:
+            print(e)
+    def _create_models(self):
+        self.metadata.create_all()
+    def _validate_connection(self):
+        print(f"State of connection is: {ibm_db.active(self.ibm_db_conn)}")
 
     def execute(self, sentence):
         self.cursor.execute(sentence)
@@ -44,8 +44,11 @@ class Db2Connection(object):
         return self.cursor.fetchone()
 
     def get_all(self, sentence):
-        self.cursor.execute(sentence)
-        return self.cursor.fetchall()
+        try:
+            self.cursor.execute(sentence)
+            return self.cursor.fetchall()
+        except Exception as e:
+            print(e)
 
     def commit(self):
         pass
