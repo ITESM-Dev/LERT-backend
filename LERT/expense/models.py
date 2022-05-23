88f77d16391077cbf45937c43927a3599f1430da
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-23 18:44:00 -0500

    feat(Expense): Expense ICA relationship DEV-148

--- LERT/expense/models.py
+++ LERT/expense/models.py
@@ -1,9 +1,15 @@
 from LERT.db.database import db
 
-association_table = db.Table(
+association_table_Expense_Resource = db.Table(
     'association', db.metadata,
     db.Column('idExpense', db.Integer, db.ForeignKey('Expense.idExpense')),
     db.Column('idResource', db.Integer, db.ForeignKey('Resource.idSerial'))
+)
+
+association_table_Expense_ICA = db.Table(
+    'association', db.metadata,
+    db.Column('idExpense', db.Integer, db.ForeignKey('Expense.idExpense')),
+    db.Column('idICA', db.Integer, db.ForeignKey('ICA.idICA'))
 )
 class Expense(db.Model):
     __tablename__ = 'Expense'
@@ -12,4 +18,6 @@ class Expense(db.Model):
     cost = db.Column(db.Float)
     date = db.Column(db.String(100))
     comment = db.Column(db.String(255))
-    resource = db.relationship("Rexource", secondary=association_table)
+    resource = db.relationship("Resource", secondary=association_table_Expense_Resource)
+    ica = db.relationship("ICA", secondary=association_table_Expense_ICA)
+    manager = db.relationship("Manager")
