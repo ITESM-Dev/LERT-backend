b2b7ec207a43d052daa4329638d1a09a5a7fe92f
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-23 16:58:58 -0500

    feat(Expense): Expense Resource relationship DEV-148

--- LERT/expense/models.py
+++ LERT/expense/models.py
@@ -1,8 +1,15 @@
 from LERT.db.database import db
 
-
+association_table = db.Table(
+    'association', db.metadata,
+    db.Column('idExpense', db.Integer, db.ForeignKey('Expense.idExpense')),
+    db.Column('idResource', db.Integer, db.ForeignKey('Resource.idSerial'))
+)
 class Expense(db.Model):
+    __tablename__ = 'Expense'
+
     idExpense = db.Column(db.Integer, primary_key=True)
     cost = db.Column(db.Float)
     date = db.Column(db.String(100))
     comment = db.Column(db.String(255))
+    resource = db.relationship("Rexource", secondary=association_table)
