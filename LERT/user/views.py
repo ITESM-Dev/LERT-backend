9560173e35c194fe0415bcb1f26427603d54c3a3
RafaGomezGlez <61569733+RafaGomezGlez@users.noreply.github.com>
2022-05-23 11:25:18 -0500

    Revert "Feature/local configuration dev 94"

--- LERT/user/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-user = Blueprint('user', __name__)
-
-@user.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-from imp import reload
-bind = ":8000"
-timeout = 3600
-loglevel= 'debug'
-workers = 4
-reload = True
