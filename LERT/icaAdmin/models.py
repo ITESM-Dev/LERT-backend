65d248fda8052601f7f8f4eacb634b5f3dbfda39
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-22 22:19:18 -0500

    ref: ICA Admin class modification-DEV-144

--- LERT/icaAdmin/models.py
+++ LERT/icaAdmin/models.py
@@ -2,7 +2,7 @@ from LERT.db.database import db
 from LERT.user.models import User
 
 
-class Administrator(db.Model):
+class ICAAdmin(db.Model):
     idICA_Admin = db.Column(db.Integer, primary_key=True)
     idUser = db.Column(db.Integer, db.ForeignKey(User.idUser))
     icaAdmin = db.relationship(User, uselist=False)
