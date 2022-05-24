4b69585fd5fc5496d2bbf2b3c680a15634227694
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-23 19:18:19 -0500

    feat(User): FK's User relationships DEV-142

--- LERT/user/models.py
+++ LERT/user/models.py
@@ -1,7 +1,8 @@
 from LERT.db.database import db
 
-
 class User(db.Model):
+    __tablename__='User'
+
     idUser = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     mail = db.Column(db.String(100), unique=True)
@@ -9,7 +10,10 @@ class User(db.Model):
     token = db.Column(db.String(100))
     expiration = db.Column(db.Integer)
     role = db.Column(db.String(100))
+    admin = db.relationship("Administrator", back_populates="user", uselist=False)
+    ica_admin = db.relationship("ICAAdmin", back_populates="user", uselist=False)
+    opManager = db.relationship("OpManager", back_populates="user", uselist=False)
+    manager = db.relationship("Manager", back_populates="user", uselist=False)
+    resource = db.relationship("Resource", back_populates="user", uselist=False)
+    
 
-def __init__(self, name, email):
-    self.name = name
-    self.email = email
