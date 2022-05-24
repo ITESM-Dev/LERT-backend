b6c0012d813c4bfc1f68d108c4c597700f1525c5
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-23 19:12:04 -0500

    feat(ResourceExpense): ResourceExpense HourType relationship DEV-149

--- LERT/resourceExpense/models.py
+++ LERT/resourceExpense/models.py
@@ -1,5 +1,11 @@
 from LERT.db.database import db
 
 class ResourceExpense(db.Model):
+    __tablename__ = 'ResourceExpense'
+    
     idResourceExpense = db.Column(db.Integer, primary_key=True)
+    idHourType = db.Column(db.Integer, db.ForeignKey("HourType.idHourType"))
+    idExpense = db.Column(db.Integer, db.ForeignKey("Expense.idExpense"))
     rate = db.Column(db.Float)
+    expense = db.relationship("Expense", back_populates="resourceExpense")
+    hourType = db.relationship("HourType", back_populates="resourceExpense")
