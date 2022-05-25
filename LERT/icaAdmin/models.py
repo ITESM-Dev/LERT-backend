5be454e7eca6d3045dc88bdb97932101d6453ee5
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-25 10:30:14 -0500

    ref(icaAdmin): FK class import DEV-162

--- LERT/icaAdmin/models.py
+++ LERT/icaAdmin/models.py
@@ -1,4 +1,5 @@
 from LERT.db.database import Base
+from LERT.user.models import User
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 
@@ -6,6 +7,6 @@ class ICAAdmin(Base):
     __tablename__='ICAAdmin'
 
     idICA_Admin = Column(Integer, primary_key=True)
-    idUser = Column(Integer, ForeignKey('User.idUser'))
+    idUser = Column(Integer, ForeignKey(User.idUser))
     user = relationship("User", back_populates="ica_admin")
     manager = relationship("Manager")
