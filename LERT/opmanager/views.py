c8d6a242ac0f2f7f2654f6e8b51451b37273c6bd
NataliaVg <natalia.velasco.ga@gmail.com>
2022-05-25 16:33:10 -0500

    ref(opmanager) Insert OPManager DEV-145

--- LERT/opmanager/views.py
+++ LERT/opmanager/views.py
@@ -1,6 +1,19 @@
 from flask import Blueprint
+from sqlalchemy.orm import Session
+from LERT.db.database import connection
+from LERT.opmanager.models import OpManager
 
 opManager = Blueprint('opManager', __name__)
+
+try:
+    session = Session(connection.e)
+
+    #opManager2 = OpManager(idUser=1, country="Mexico", status="Active")
+    #session.add(opManager2)
+    #session.commit() 
+    session.close()
+except Exception as e:
+    print(e)
 
 @opManager.route("/")
 def hello():
