9459b58a5fbbcad33167bae1c2807aea1fdaf434
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-25 10:30:57 -0500

    ref(session): Change in connection placement DEV-162

--- LERT/db/session.py
+++ LERT/db/session.py
@@ -3,13 +3,15 @@ from LERT.db.database import connection
 from LERT.administrator.models import Administrator
 
 try:
+
     session = Session(connection.e)
-    admin1 = Administrator(name="admin1")
-    session.add(admin1)
-    session.commit() 
+    #admin1 = Administrator(name="admin1")
+    #session.add(admin1)
+    #session.commit() 
+
+    connection.Base.metadata.create_all(connection.e)
+    print(connection.Base.metadata.sorted_tables)
 
 except Exception as e:
     print(e)
-
-    connection.Base.metadata.create_all(connection.e)
 
