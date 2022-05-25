5e0281b6631aee786e317e178eb66baf650a637f
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-25 10:30:03 -0500

    ref(expense): FK class import DEV-162

--- LERT/expense/models.py
+++ LERT/expense/models.py
@@ -1,24 +1,29 @@
 from LERT.db.database import Base
+from LERT.expense.models import Expense
+from LERT.resource.models import Resource
+from LERT.manager.models import Manager
+from LERT.expenseType.models import ExpenseType
+from LERT.ica.models import ICA
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 
 association_table_Expense_Resource = Table(
     'association', Base.metadata,
-    Column('idExpense', Integer, ForeignKey('Expense.idExpense')),
-    Column('idResource', Integer, ForeignKey('Resource.idSerial'))
+    Column('idExpense', Integer, ForeignKey(Expense.idExpense)),
+    Column('idResource', Integer, ForeignKey(Resource.idSerial))
 )
 
 association_table_Expense_ICA = Table(
     'association', Base.metadata,
-    Column('idExpense', Integer, ForeignKey('Expense.idExpense')),
-    Column('idICA', Integer, ForeignKey('ICA.idICA'))
+    Column('idExpense', Integer, ForeignKey(Expense.idExpense)),
+    Column('idICA', Integer, ForeignKey(ICA.idICA))
 )
 class Expense(Base):
     __tablename__ = 'Expense'
 
     idExpense = Column(Integer, primary_key=True)
-    idManager = Column(Integer, ForeignKey('Manager.idManager'))
-    idExpenseType = Column(Integer, ForeignKey('ExpenseType.idExpenseType'))
+    idManager = Column(Integer, ForeignKey(Manager.idManager))
+    idExpenseType = Column(Integer, ForeignKey(ExpenseType.idExpenseType))
     cost = Column(Float)
     date = Column(String(100))
     comment = Column(String(255))
