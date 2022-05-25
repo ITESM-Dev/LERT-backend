558a682283564fa60d614bc8fa6434a497cb42dd
NataliaVg <natalia.velasco.ga@gmail.com>
2022-05-25 16:32:08 -0500

    ref(user) Insert user DEV-142

--- LERT/user/views.py
+++ LERT/user/views.py
@@ -6,12 +6,16 @@ from LERT.user.models import User
 
 user = Blueprint('user', __name__)
 
-session = Session(connection.e)
+try:
+    session = Session(connection.e)
 
-user1 = User(name = "test name 2", mail = "fffd@test.com", password = "testpassword",
-token = "testToken", expiration = 8, role = "Admin")
-session.add(user1)
-session.commit() 
+#    user1 = User(name = "test name 2", mail = "fffd@test.com", password = "testpassword",
+#    token = "testToken", expiration = 8, role = "Admin")
+#    session.add(user1)
+#    session.commit() 
+    session.close()
+except Exception as e:
+    print(e)
 
 @user.route("/")
 def hello():
