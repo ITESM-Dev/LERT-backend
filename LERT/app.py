617f10d7229c0144bdf2d64b251585aff807308b
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-25 14:37:28 -0500

    feat(app): Blueprints registry DEV-162

--- LERT/app.py
+++ LERT/app.py
@@ -2,6 +2,12 @@ from flask import jsonify, Flask
 from sqlalchemy import *
 from LERT.db import database, session
 from LERT.user.views import user
+from LERT.administrator.views import admin
+from LERT.icaAdmin.views import icaAdmin
+from LERT.resource.views import resource
+from LERT.expense.views import expense
+from LERT.resourceExpense.views import resourceExpense
+
 from db2_Connection import Db2Connection
 
 app = Flask(__name__)
@@ -12,6 +18,11 @@ def create_app():
     session
   
 app.register_blueprint(user)
+app.register_blueprint(admin)
+app.register_blueprint(icaAdmin)
+app.register_blueprint(resource)
+app.register_blueprint(expense)
+app.register_blueprint(resourceExpense)
 
 #sentence = "SELECT * FROM OOLONG"
 #rows = connection.get_all(sentence)
