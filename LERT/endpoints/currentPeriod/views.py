10abd2b695d2a3af6d7c7e5cdbb2c5e3de9b9712
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-31 18:17:19 -0500

    feat(currentPeriod): Create Current Period DEV-224

--- LERT/endpoints/currentPeriod/views.py
+++ LERT/endpoints/currentPeriod/views.py
@@ -1,7 +1,40 @@
+from crypt import methods
 from flask import Blueprint
+import flask_login
+from flask_cors import cross_origin
+import flask
+from sqlalchemy.orm import Session
+from LERT.db.database import connection
+import requests
+from LERT.endpoints.currentPeriod.models import CurrentPeriod
 
 currentPeriod = Blueprint('currentPeriod', __name__)
 
-@currentPeriod.route("/")
-def hello():
-    return "hello"
+session = Session(connection.e)
+
+@currentPeriod.route("/createCurrentPeriod", methods = ['POST'])
+@cross_origin()
+@flask_login.login_required
+def createCurrentPeriod():
+
+    statusCode = flask.Response(status=201)
+    quarterReq = int(flask.request.json['quarter'])
+    yearReq = int(flask.request.json['year'])
+    keyReq = int(flask.request.json['key'])
+    statusReq = flask.request.json['status']
+
+    try:
+        
+        currentPeriod1 = CurrentPeriod(quarter = quarterReq, year = yearReq, key = keyReq, status = statusReq)
+        session.add(currentPeriod1)
+        session.commit() 
+        
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+
+
+    return statusCode
+
+session.close()
