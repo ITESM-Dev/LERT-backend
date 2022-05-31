3b813ad6571d61a0cd1cb841b54c81ca3531eeef
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-31 18:16:14 -0500

    ref(db): Commented database connection DEV-224

--- LERT/db/database.py
+++ LERT/db/database.py
@@ -2,8 +2,8 @@ import sys
 sys.path.insert(0,'./lert_driver_db2/db2')
 from db2_Connection import Db2Connection
 from sqlalchemy import *
-
+"""
 connection = Db2Connection()
 connection._create_connection_sqlAlchemy()
 Base = connection.Base
-
+"""
