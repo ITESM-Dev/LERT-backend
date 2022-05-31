5368edc8ededac3931c37f087fa72f04727d33ae
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-31 10:42:35 -0500

    ref(user): Erros email already exists DEV-224

--- LERT/user/views.py
+++ LERT/user/views.py
@@ -45,7 +45,7 @@ def createUser():
         session.commit() 
         
     except Exception as e:
-        return "Email already exists", 401
+        print(e)
 
     userDB = session.query(User).filter_by(mail = userMail).first()
 
