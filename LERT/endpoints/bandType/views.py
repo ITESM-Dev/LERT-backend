0d360870c561c9a6b36b2c254ebc5cfcc6e28a4b
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-31 18:16:44 -0500

    feat(bandType): Create BandType endpoint DEV-224

--- LERT/endpoints/bandType/views.py
+++ LERT/endpoints/bandType/views.py
@@ -1,7 +1,40 @@
+from crypt import methods
 from flask import Blueprint
-
+import flask_login
+from flask_cors import cross_origin
+import flask
+from sqlalchemy.orm import Session
+from LERT.db.database import connection
+from LERT.endpoints.bandType.models import BandType
+import requests
 bandType = Blueprint('bandType', __name__)
 
-@bandType.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+session = Session(connection.e)
+
+@bandType.route("/createBandType", methods=['POST'])
+@cross_origin()
+@flask_login.login_required
+def createBandType():
+
+    statusCode = flask.Response(status=201)
+    typeReq = flask.request.json['type']
+    bandReq = flask.request.json['band'] 
+    yearlyRateReq = int(flask.request.json['yearlyRate'])
+    countryReq = flask.request.json['country']
+    dateToStartReq = flask.request.json['dateToStart']
+
+    try:
+        session = Session(connection.e)
+
+        bandType1 = BandType(type = typeReq, band = bandReq, yearlyRate = yearlyRateReq, country = countryReq, dateToStart = dateToStartReq)
+        session.add(bandType1)
+        session.commit() 
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)    
+    except Exception as e:
+        print(e)
+
+    return statusCode
+
+session.close()
+
