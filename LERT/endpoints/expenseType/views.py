ffa51513e7169dc5a7ee8837b2fbda3b62a8506f
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-05-31 18:18:01 -0500

    feat(expenseType): Create Expense Type DEV-224

--- LERT/endpoints/expenseType/views.py
+++ LERT/endpoints/expenseType/views.py
@@ -1,7 +1,36 @@
 from flask import Blueprint
+import flask_login
+from flask_cors import cross_origin
+import flask
+from sqlalchemy.orm import Session
+from LERT.db.database import connection
+import requests
+from LERT.endpoints.expenseType.models import ExpenseType
 
 expenseType = Blueprint('expenseType', __name__)
 
-@expenseType.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+session = Session(connection.e)
+
+@expenseType.route("/createExpenseType", methods=['POST'])
+@cross_origin()
+@flask_login.login_required
+def createExpenseType():
+
+    statusCode = flask.Response(status=201)
+    typeReq = flask.request.json['type']
+
+    try:
+        
+        expenseType1 = ExpenseType(type = typeReq)
+        session.add(expenseType1)
+        session.commit() 
+        
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+        
+    return statusCode
+
+
+session.close()
