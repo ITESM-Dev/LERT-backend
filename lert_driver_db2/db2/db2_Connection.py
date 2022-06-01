a694e65df91b142d81c7a778807e34423535d3bb
RafaGomezGlez <A01733983@itesm.mx>
2022-05-31 21:41:09 -0500

    ref: Separating env

--- lert_driver_db2/db2/db2_Connection.py
+++ lert_driver_db2/db2/db2_Connection.py
@@ -1,4 +1,5 @@
 import ibm_db
+import ibm_db_dbi
 import os
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
@@ -10,17 +11,16 @@ DB_HOSTNAME = os.environ.get("DB_HOSTNAME")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 UID = os.environ.get("UID")
 DB_PORT = os.environ.get("DB_PORT")
-
+ENVIRONMENT = os.environ.get('ENVIRONMENT')
 class Db2Connection(object):        
 
     def _create_connection_sqlAlchemy(self):
         try:
-            if os.environ.get('ENVIRONMENT') == "dev":
+            if ENVIRONMENT == "dev":
                 db_string = f"db2+ibm_db://{UID}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}"
-            elif os.environ.get('ENVIRONMENT') == "prod":
+            elif ENVIRONMENT == "prod":
                 db_string = f"db2+ibm_db://{UID}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME};SECURITY=SSL"
             self.e = create_engine(db_string)
-            self.e.connect()
             self.metadata = MetaData()
             self.Base = declarative_base(metadata=self.metadata)
             self.metadata.bind = self.e
