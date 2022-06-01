ee22807cb6885b2defe1809a51a35caf5237d49d
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-06-01 16:11:31 -0500

    feat(dbCreation): Creation of DB tables

---
+++ LERT/db/dbCreation.py
@@ -0,0 +1,21 @@
+from LERT.db.database import connection
+from LERT.endpoints.administrator.models import Administrator
+from LERT.endpoints.bandType.models import BandType
+from LERT.endpoints.expense.models import Expense
+from LERT.endpoints.expenseType.models import ExpenseType
+from LERT.endpoints.hourType.models import HourType
+from LERT.endpoints.ica.models import ICA
+from LERT.endpoints.icaAdmin.models import ICAAdmin
+from LERT.endpoints.manager.models import Manager
+from LERT.endpoints.opmanager.models import OpManager
+from LERT.endpoints.resource.models import Resource
+from LERT.endpoints.resourceExpense.models import ResourceExpense
+from LERT.endpoints.user.models import User
+from LERT.endpoints.currentPeriod.models import CurrentPeriod
+try:
+
+    connection.Base.metadata.create_all(connection.e)
+    print(connection.Base.metadata.tables)
+
+except Exception as e:
+    print(e)
