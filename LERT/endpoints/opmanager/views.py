8d740c8c0f3fe428365cb10a63f9c7fd73955e35
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-06-02 17:17:33 -0500

    feat(opManager): Update & delete Band Types endpoints DEV-215

--- LERT/endpoints/opmanager/views.py
+++ LERT/endpoints/opmanager/views.py
@@ -4,10 +4,12 @@ from flask import Blueprint, jsonify
 from sqlalchemy.orm import Session
 from LERT.db.database import connection
 from LERT.endpoints.opmanager.models import OpManager
+from LERT.endpoints.bandType.models import BandType
 from flask_cors import cross_origin
 import requests
 import flask_login
-from LERT.endpoints.bandType.models import BandType
+import flask
+import datetime
 
 opManager = Blueprint('opManager', __name__)
 
@@ -38,5 +40,55 @@ def getBandTypes():
         raise SystemExit(e)        
     except Exception as e:
         print(e)
+
+@opManager.route("/updateBandType", methods=['POST'])
+@cross_origin()
+@flask_login.login_required
+def updateBandTypes():
+    try:
+        bandTypeIdReq = int(flask.request.json['id'])
+        typeReq = flask.request.json['type']
+        bandReq = flask.request.json['band']
+        countryReq = flask.request.json['country']
+        yearlyRateReq = int(flask.request.json['yearlyRate'])
+        dateToStartReq = flask.request.json['dateToStart']
+        dateToFinishReq = flask.request.json['dateToFinish']
+
+        y, m, d = dateToStartReq.split('-')
+        startDateReq = datetime.datetime(int(y), int(m), int(d))
+
+        y, m, d = dateToFinishReq.split('-')
+        endDateReq = datetime.datetime(int(y), int(m), int(d))
+
+        session.query(BandType).\
+            filter_by(idBandType = bandTypeIdReq).\
+            update({BandType.type: typeReq, BandType.country:countryReq, BandType.band:bandReq,
+            BandType.yearlyRate:yearlyRateReq, BandType.dateToStart: startDateReq, BandType.dateToFinish: endDateReq})
+
+        session.commit()
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e) 
+
+    return "Band Type Updated", 200
+
+@opManager.route("/deleteBandType", methods=['POST'])
+@cross_origin()
+@flask_login.login_required
+def deleteBandTypes():
+    try:
+        bandTypeIdReq = int(flask.request.json['id'])
+        bandTypeDb = session.query(BandType).filter_by(idBandType = bandTypeIdReq).first()
+        bandTypeDb.delete()
+        session.commit()
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e) 
+
+    return "Band Type Deleted", 200
 
 session.close()
