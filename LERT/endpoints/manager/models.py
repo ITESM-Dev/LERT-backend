5095b5f63d2d1af92e97a8d70d6cfee2b0656bc2
NataliaVg <natalia.velasco.ga@gmail.com>
2022-06-02 08:37:21 -0500

    ref(manager) Add col to DB

--- LERT/endpoints/manager/models.py
+++ LERT/endpoints/manager/models.py
@@ -10,6 +10,9 @@ class Manager(Base):
     idOPManager = Column(Integer, ForeignKey('OpManager.idOPManager'))
     idICA_Admin = Column(Integer, ForeignKey('ICAAdmin.idICA_Admin'))
     recoveryStatus = Column(String(30))
+    status = Column(String(30))
+    lastUpdated = Column(Date)
+
 
     user = relationship("User", back_populates="manager")
     expense = relationship("Expense")
