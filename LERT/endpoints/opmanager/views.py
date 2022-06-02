62f33f831cefa6f407afb57ec4fa99e88d2d5989
NataliaVg <natalia.velasco.ga@gmail.com>
2022-06-02 12:16:46 -0500

    feat(opmanager) Endpoints: getManagers, update manager DEV-214

--- LERT/endpoints/opmanager/views.py
+++ LERT/endpoints/opmanager/views.py
@@ -1,20 +1,81 @@
-from flask import Blueprint
+from sys import stderr
+from flask import Blueprint, jsonify
+import flask
+from flask_cors import cross_origin
+import flask_login
+import requests
 from sqlalchemy.orm import Session
 from LERT.db.database import connection
+from LERT.endpoints.manager.models import Manager
 from LERT.endpoints.opmanager.models import OpManager
+from LERT.endpoints.user.models import User
 
 opManager = Blueprint('opManager', __name__)
 
-try:
-    session = Session(connection.e)
+session = Session(connection.e)
 
-    #opManager2 = OpManager(idUser=1, country="Mexico", status="Active")
-    #session.add(opManager2)
-    #session.commit() 
-    session.close()
-except Exception as e:
-    print(e)
+@opManager.route("/getManagers", methods=['GET'])
+@cross_origin()
+@flask_login.login_required
+def getManagers():
+    try:
+        opManagerMail = flask_login.current_user.id
+        opManagerUserID =  session.query(User).filter_by(mail = opManagerMail).first().idUser
+        opManagerID = session.query(OpManager).filter_by(idUser = opManagerUserID).first().idOPManager
+        managers = session.query(Manager).filter_by(idOPManager = opManagerID).all()
 
-@opManager.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+        resultManagers = []
+
+        for current in managers:
+
+            currentMail =  session.query(User).get(current.idUser).mail
+            
+            currentManager = {
+                "id": current.idManager,
+                "mail": currentMail,
+                "status": current.status,
+                "recoveryStatus": current.recoveryStatus,
+                "lastUpdated": current.lastUpdated
+            }
+
+            resultManagers.append(currentManager)
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+
+    return jsonify(resultManagers), 200    
+
+@opManager.route("/updateManager", methods=['PATCH'])
+@cross_origin()
+@flask_login.login_required
+def updateManager():
+    try:
+        statusCode = flask.Response(status=201)
+        managerMailReq = flask.request.json['mail']
+        managerStatusReq = flask.request.json['status']
+        opManagerMail = flask_login.current_user.id
+
+        managerUserID = session.query(User).filter_by(mail = managerMailReq).first().idUser
+        managerOpManID = session.query(Manager).filter_by(idUser = managerUserID).first().idOPManager
+        opManagerUserID = session.query(User).filter_by(mail = opManagerMail).first().idUser
+        opManagerID = session.query(OpManager).filter_by(idUser = opManagerUserID).first().idOPManager
+
+        if managerOpManID == opManagerID:
+            managerDBQuery =  session.query(Manager).filter_by(idUser = managerUserID)
+
+            managerDBQuery.\
+                update({Manager.status: managerStatusReq}, synchronize_session='fetch')
+
+            session.commit()  
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+
+    return statusCode  
+    
+
+session.close()
