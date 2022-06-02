55b22d20aa3a680a31171ec8ea8d8b7300f9a776
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-06-02 11:06:37 -0500

    feat(opManager): Get Band Types endpoint DEV-215

--- LERT/endpoints/opmanager/views.py
+++ LERT/endpoints/opmanager/views.py
@@ -1,20 +1,42 @@
-from flask import Blueprint
+from crypt import methods
+from sys import stderr
+from flask import Blueprint, jsonify
 from sqlalchemy.orm import Session
 from LERT.db.database import connection
 from LERT.endpoints.opmanager.models import OpManager
+from flask_cors import cross_origin
+import requests
+import flask_login
+from LERT.endpoints.bandType.models import BandType
 
 opManager = Blueprint('opManager', __name__)
 
-try:
-    session = Session(connection.e)
+session = Session(connection.e)
 
-    #opManager2 = OpManager(idUser=1, country="Mexico", status="Active")
-    #session.add(opManager2)
-    #session.commit() 
-    session.close()
-except Exception as e:
-    print(e)
+@opManager.route("/getBandTypes", methods=['GET'])
+@cross_origin()
+@flask_login.login_required
+def getBandTypes():
+    try:
+        bandTypesDB = session.query(BandType).all()
+        bandTypes = []
+        for band in bandTypesDB:
+            currentBand = {
+                "id" : band.idBandType,
+                "type": band.type,
+                "country": band.country,
+                "band": band.band,
+                "yearlyRate": band.yearlyRate,
+                "dateToStart": str(band.dateToStart),
+                "dateToFinish": str(band.dateToFinish)
+            }
+            bandTypes.append(currentBand)
 
-@opManager.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+        return jsonify(bandTypes)
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+
+session.close()
