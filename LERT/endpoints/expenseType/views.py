e7d2dc8df3e5aa99046961bd76d33614838e9dd3
NataliaVg <natalia.velasco.ga@gmail.com>
2022-06-03 18:03:31 -0500

    feat(expenseType) Endpoints: get, update and delet expense types DEV-216

--- LERT/endpoints/expenseType/views.py
+++ LERT/endpoints/expenseType/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 import flask_login
 from flask_cors import cross_origin
 import flask
@@ -29,10 +29,72 @@ def createExpenseType():
         raise SystemExit(e)        
     except Exception as e:
         print(e)
+        
+    return statusCode
 
-    id = {"id": expenseType1.idExpenseType }
-    
-    return id, 201   
+@expenseType.route("/getExpenseTypes", methods=['GET'])
+@cross_origin()
+@flask_login.login_required
+def getExpenseType():
+    try:
+        expenseTypes = session.query(ExpenseType).all()
 
+        resultTypes = []
+
+        for current in expenseTypes:
+            
+            currentManager = {
+                "idExpenseType": current.idExpenseType,
+                "type": current.type
+            }
+
+            resultTypes.append(currentManager)
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+    return jsonify(resultTypes), 200
+
+@expenseType.route("/updateExpenseType", methods=['POST'])
+@cross_origin()
+@flask_login.login_required
+def updateExpenseType():
+    try:
+
+        idExpenseTypeReq = flask.request.json['id']
+        expenseTypeReq = flask.request.json['type']
+
+        expenseTypeQuery = session.query(ExpenseType).filter_by(idExpenseType = idExpenseTypeReq)
+        
+        expenseTypeQuery.\
+        update({ExpenseType.type: expenseTypeReq}, synchronize_session='fetch')
+
+        session.commit()
+        
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+
+    return "Expense Type updated" ,200
+
+@expenseType.route("/deleteExpenseType", methods=['POST'])
+@cross_origin()
+@flask_login.login_required
+def deleteExpenseType():
+    try:
+        idExpenseTypeReq = flask.request.json['id']
+
+        expenseTypeQuery = session.query(ExpenseType).filter_by(idExpenseType = idExpenseTypeReq).first()
+        session.delete(expenseTypeQuery)
+        session.commit()
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)        
+    except Exception as e:
+        print(e)
+
+    return "Expense Type deleted" ,200
 
 session.close()
