b9bbc3d057c7d18d8ef6032ab2eea9f5ac9b9cfa
Brenda1605 <59668172+Brenda1605@users.noreply.github.com>
2022-06-07 19:09:54 -0500

    ref(auth): Manager OpManager permission DEV-214

--- LERT/endpoints/authorization/roles.py
+++ LERT/endpoints/authorization/roles.py
@@ -5,4 +5,5 @@ opManager_permission = Permission(RoleNeed('OpManager'))
 manager_permission = Permission(RoleNeed('Manager'))
 icaAdmin_permission = Permission(RoleNeed('IcaAdmin'))
 manager_or_IcaAdmin = Permission(RoleNeed('Manager'), RoleNeed('IcaAdmin'))
+manager_or_OpManager = Permission(RoleNeed('Manager'), RoleNeed('OpManager'))
 
