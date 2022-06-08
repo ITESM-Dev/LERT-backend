f83d8202b952cfc694977bf36ec739a3fc109cbb
NataliaVg <natalia.velasco.ga@gmail.com>
2022-06-08 13:12:16 -0500

    ref(app) Add permissions DEV-251

--- LERT/endpoints/authorization/roles.py
+++ LERT/endpoints/authorization/roles.py
@@ -6,4 +6,6 @@ manager_permission = Permission(RoleNeed('Manager'))
 icaAdmin_permission = Permission(RoleNeed('IcaAdmin'))
 manager_or_IcaAdmin = Permission(RoleNeed('Manager'), RoleNeed('IcaAdmin'))
 manager_or_OpManager = Permission(RoleNeed('Manager'), RoleNeed('OpManager'))
+manager_or_OpManager_or_icaAdmin = Permission(RoleNeed('Manager'), RoleNeed('OpManager'), RoleNeed('IcaAdmin'))
+
 
