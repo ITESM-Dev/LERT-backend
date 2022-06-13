81ad173633a23f76e86cfe40654e7d82db968527
RafaGomezGlez <A01733983@itesm.mx>
2022-06-13 14:20:13 -0500

    ref: Deleting unnecessary endpoints

--- LERT/endpoints/resource/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-resource = Blueprint('resource', __name__)
-
-@resource.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
--- LERT/endpoints/resourceExpense/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-resourceExpense = Blueprint('resourceExpense', __name__)
-
-@resourceExpense.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
